9c1b5022032750993cf4628a7476d412c1514003
Victor <viggo00@hotmail.com>
2021-03-11 11:49:07 +0100

    slight cleanup

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.message == 'capture') {
-		chrome.tabs.captureVisibleTab(chrome.windows.WINDOW_ID_CURRENT, function(dataUrl) {
-			sendResponse({imgSrc: dataUrl, message: 'done'});
+		chrome.tabs.captureVisibleTab(chrome.windows.WINDOW_ID_CURRENT, function(dataUri) {
+			sendResponse({imgSrc: dataUri, message: 'done'});
 			return true;
 		});
 	}
--- content.js
+++ content.js
@@ -2,7 +2,7 @@ var toggled = false;
 var timeout = 5;
 var interval;
 var latestCoords;
-var lastCoords = [0,0];
+var lastCoords = [0, 0];
 var textField;
 
 let tempCanvas = document.createElement('canvas');
@@ -43,21 +43,21 @@ function logPosition(e) {
 
 function grabColor() {
     timeout = timeout - 1;
-    if (timeout < 0){
-        if (latestCoords[0] != lastCoords[0] || latestCoords[1] != lastCoords[1]){
+    if (timeout < 0) {
+        if (latestCoords[0] != lastCoords[0] || latestCoords[1] != lastCoords[1]) {
             lastCoords = latestCoords.slice(0); //clone latestcoords
             //console.log(latestCoords);
 
-            chrome.runtime.sendMessage({message: 'capture'}, (currentScreen) => {
+            chrome.runtime.sendMessage({ message: 'capture' }, (currentScreen) => {
                 //console.log(currentScreen.imgSrc);
                 let img = new Image();
                 img.src = currentScreen.imgSrc;
-                img.onload = function() {
+                img.onload = function () {
                     tempCanvas.width = img.naturalWidth;
                     tempCanvas.height = img.naturalHeight;
-        
+
                     tempContext.drawImage(img, 0, 0);
-        
+
                     // Use pixel ratio to make sure it works on screens with very high resolutions like Retina
                     getColor(latestCoords[0] * window.devicePixelRatio, latestCoords[1] * window.devicePixelRatio);
                 }
@@ -67,29 +67,29 @@ function grabColor() {
 }
 
 function getColor(x, y) {
-	let pixel = tempContext.getImageData(x, y, 1, 1).data;
-	let red = pixel[0];
-	let green = pixel[1];
-	let blue = pixel[2];
+    let pixel = tempContext.getImageData(x, y, 1, 1).data;
+    let red = pixel[0];
+    let green = pixel[1];
+    let blue = pixel[2];
 
-	//console.log(`${red},${green},${blue}`);
+    //console.log(`${red},${green},${blue}`);
     let hexColor = rgbToHex(red, green, blue);
 
-    chrome.runtime.sendMessage({message: 'getColorName', data: hexColor}, (response) => {
+    chrome.runtime.sendMessage({ message: 'getColorName', data: hexColor }, (response) => {
         console.log(response.name);
         colorName = response.name;
         textField.innerText = `${red},${green},${blue} \n ${hexColor.toUpperCase()} \n ${colorName[1]}`;
     });
-    
 
-    
+
+
 }
 
 function componentToHex(c) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
-  }
-  
-  function rgbToHex(r, g, b) {
+}
+
+function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-  }
+}
